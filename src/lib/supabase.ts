defb01957e61b77b5e4214353adf7474e583d2de
zouhaipeng.721 <zouhaipeng.721@bytedance.com>
2025-04-11 16:48:24 +0800

    init

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from '../types/supabase';
 
 if (!import.meta.env.VITE_SUPABASE_URL) {
-  throw new Error('Missing environment variable: VITE_SUPABASE_URL');
+  throw new Error('Missing environment variable : VITE_SUPABASE_URL');
 }
 if (!import.meta.env.VITE_SUPABASE_ANON_KEY) {
   throw new Error('Missing environment variable: VITE_SUPABASE_ANON_KEY');
